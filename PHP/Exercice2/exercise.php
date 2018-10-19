30864a59aa36a26db6baa015ed8b98e4c7180224
DiegoSaldivar <dssaldivar@hotmail.com>
2018-10-19 11:12:02 +0200

    DiegoSaldivar_exo_2

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,7 +2,17 @@
 $password;
 $salt;
 #echo ceil(strlen($password) / 2);
-$pos = floor(strlen($password) / 2);
+/* $pos = floor(strlen($password) / 2);
 $firstPart = substr($password, 0, $pos);
 $lastPart = substr($password, $pos, strlen($password) - 1);
+$saltedPassword = $firstPart . $salt . $lastPart;
+ */
+
+$firstPart = substr(
+    $password,
+    0,
+    floor(strlen($password) / 2) + (strlen($password) % 2)
+);
+
+$lastPart = substr($password, ceil(strlen($password) / 2));
 $saltedPassword = $firstPart . $salt . $lastPart;
