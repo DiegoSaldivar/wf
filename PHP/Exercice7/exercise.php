b6b77c09debf52215f977aca50e3f2c9b92754df
DiegoSaldivar <dssaldivar@hotmail.com>
2018-10-22 17:02:40 +0200

    DiegoSaldivar_PHP_exo_7

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,28 @@
 <?php
+
+function divide(int $base, int $by) : float {
+
+    if($by == 0){
+        throw new RuntimeException('Division by 0 is not allowed');
+    }
+    return $base/$by;
+}
+
+
+function arrayDivide(array $baseArray, int $by) : array {
+    $result = [];
+    
+    foreach ($baseArray as $base) {
+        try{
+            $result[] = divide($base, $by);
+        } catch ( RuntimeException $exception){
+            $result[] = $base;
+        }
+        
+    }
+    return $result;
+}
+
+
+
+
